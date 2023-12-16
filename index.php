c182c7e72e0b7c8f1497f0955c9111d3239b9f21
kjdutaro <kjdutaro@gmail.com>
2023-12-16 23:43:55 +0800

    fin commit

--- index.php
+++ index.php
@@ -3,31 +3,193 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
     <title>Database Queries</title>
 </head>
 <body>
     <h1>Database Queries</h1>
 
-    <h2>1. Join Query</h2>
-    <?php include('join_query.php'); ?>
+    <form method="post">
+        <label for="querySelector">Select Filter:</label>
+        <select id="querySelector" name="query">
+            <option value="join">Join</option>
+            <option value="leftJoin">Left Join</option>
+            <option value="rightJoin">Right Join</option>
+            <option value="subquery">Subquery</option>
+            <option value="procedure">Stored Procedure</option>
+            <option value="function">Stored Function</option>
+        </select>
+        <input type="submit" value="Filter">
+    </form>
 
-    <h2>2. Left Join Query</h2>
-    <?php include('left_join_query.php'); ?>
+    <?php
+    include('db_conn.php');
 
-    <h2>3. Right Join Query</h2>
-    <?php include('right_join_query.php'); ?>
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $selectedQuery = $_POST['query'];
 
-    <h2>4. Subquery</h2>
-    <?php include('subquery.php'); ?>
+        switch ($selectedQuery) {
+            case 'join':
+                $query = "SELECT users.username, orders.total_amount
+                          FROM users
+                          INNER JOIN orders ON users.id = orders.user_id";
 
-    <h2>5. Stored Procedure</h2>
-    <a href="create_procedure.php">Create Procedure</a>
-    <br>
-    <a href="use_procedure.php">Use Procedure</a>
+                $result = $conn->query($query);
 
-    <h2>6. Stored Function</h2>
-    <a href="create_function.php">Create Function</a>
-    <br>
-    <a href="use_function.php">Use Function</a>
+                if ($result->num_rows > 0) {
+                    echo '<ul>';
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<li>' . $row['username'] . ' - ' . $row['total_amount'] . '</li>';
+                    }
+                    echo '</ul>';
+                } else {
+                    echo 'No results found.';
+                }
+
+                break;
+
+            case 'leftJoin':
+                $query = "SELECT users.username, orders.total_amount
+                          FROM users
+                          LEFT JOIN orders ON users.id = orders.user_id";
+
+                $result = $conn->query($query);
+
+                if ($result->num_rows > 0) {
+                    echo '<ul>';
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<li>' . $row['username'] . ' - ' . $row['total_amount'] . '</li>';
+                    }
+                    echo '</ul>';
+                } else {
+                    echo 'No results found.';
+                }
+
+                break;
+
+            case 'rightJoin':
+                $query = "SELECT users.username, orders.total_amount
+                          FROM users
+                          RIGHT JOIN orders ON users.id = orders.user_id";
+
+                $result = $conn->query($query);
+
+                if ($result->num_rows > 0) {
+                    echo '<ul>';
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<li>' . $row['username'] . ' - ' . $row['total_amount'] . '</li>';
+                    }
+                    echo '</ul>';
+                } else {
+                    echo 'No results found.';
+                }
+
+                break;
+
+            case 'subquery':
+                $query = "SELECT username, total_amount
+                          FROM (
+                              SELECT users.username, orders.total_amount
+                              FROM users
+                              INNER JOIN orders ON users.id = orders.user_id
+                          ) AS subquery";
+
+                $result = $conn->query($query);
+
+                if ($result->num_rows > 0) {
+                    echo '<ul>';
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<li>' . $row['username'] . ' - ' . $row['total_amount'] . '</li>';
+                    }
+                    echo '</ul>';
+                } else {
+                    echo 'No results found.';
+                }
+
+                break;
+
+            case 'procedure':
+                $createProcedureSQL = "
+                DELIMITER //
+                
+                CREATE PROCEDURE proc()
+                BEGIN
+                    SELECT emp_no, first_name, last_name
+                    FROM employees
+                    WHERE gender = 'M';
+                END //
+                
+                DELIMITER ;
+                ";
+
+                if ($conn->multi_query($createProcedureSQL)) {
+                    echo 'Stored procedure created successfully.';
+                } else {
+                    echo 'Error creating stored procedure: ' . $conn->error;
+                }
+
+                $procedureCall = "CALL proc()";
+
+                if ($conn->multi_query($procedureCall)) {
+                    do {
+                        if ($result = $conn->store_result()) {
+                            while ($row = $result->fetch_assoc()) {
+                                echo '<p>' . $row['emp_no'] . ' - ' . $row['first_name'] . ' ' . $row['last_name'] . '</p>';
+                            }
+                            $result->free();
+                        }
+                    } while ($conn->more_results() && $conn->next_result());
+                } else {
+                    echo 'Error calling stored procedure: ' . $conn->error;
+                }
+
+                break;
+
+            case 'function':
+                $createFunctionSQL = "
+                DELIMITER //
+                
+                CREATE FUNCTION dbfunct()
+                RETURNS INT
+                BEGIN
+                    DECLARE totalEmployees INT;
+
+                    SELECT COUNT(*) INTO totalEmployees FROM employees;
+
+                    RETURN totalEmployees;
+                END //
+                
+                DELIMITER ;
+                ";
+
+                if ($conn->multi_query($createFunctionSQL)) {
+                    echo 'Stored function created successfully.';
+                } else {
+                    echo 'Error creating stored function: ' . $conn->error;
+                }
+
+                $functionCall = "SELECT dbfunct() AS result";
+
+                $result = $conn->query($functionCall);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<p>Total Employees: ' . $row['result'] . '</p>';
+                    }
+                } else {
+                    echo 'No results found.';
+                }
+
+                break;
+
+            default:
+                echo 'Invalid query selection';
+        }
+    }
+
+    $conn->close();
+    ?>
+
 </body>
 </html>
